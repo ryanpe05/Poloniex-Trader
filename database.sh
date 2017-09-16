0d4078eb3a6d2b07895aa0a6db2aaeb52ea9cda0
Ryan Peck <ryanpeck239@gmail.com>
2017-09-16 13:43:46 -0400

    Standard Deviation Works
    This should help us establish Bollinger Bands and start to look for
    breakout movements in either direction.

--- database.sh
+++ database.sh
@@ -1,5 +1,5 @@
 echo "Creating database, and setting up tables."
 echo "Please login to mysql"
-mysql -u root -p  -Bse "create database crypto; use crypto; create table trades (id int NOT NULL AUTO_INCREMENT, amount  float, rate float, type varchar(5), date date, date selldate, PRIMARY KEY (id)); create table mytrades(id int NOT NULL AUTO_INCREMENT, amount  float, rate float, type varchar(5), date date, date selldate, PRIMARY KEY (id)); create user 'trader'@'localhost' IDENTIFIED BY 'Cf2w[\bSf4mRYU.b'; GRANT INSERT, DROP, SELECT, UPDATE on *.* to 'trader'@'localhost'; FLUSH PRIVILEGES;"
+mysql -u root -p  -Bse "create database crypto; use crypto; create table trades (id int NOT NULL AUTO_INCREMENT, amount  float, rate float, type varchar(5), date date, selldate date, PRIMARY KEY (id)); create table mytrades(id int NOT NULL AUTO_INCREMENT, amount  float, rate float, type varchar(5), date date, selldate date, PRIMARY KEY (id)); create user 'trader'@'localhost' IDENTIFIED BY 'Cf2w[\bSf4mRYU.b'; GRANT INSERT, DROP, SELECT, UPDATE on *.* to 'trader'@'localhost'; FLUSH PRIVILEGES;"
 
 echo "Unless I printed errors, everything probably worked"
